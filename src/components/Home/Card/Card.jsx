ed005dadc73a370c52b7bd11213ce18dc8b45ad3
unknown <ruslan.zhumabaev1234@gmail.com>
2021-08-23 15:36:15 +0600

    component card

--- src/components/Home/Card/Card.jsx
+++ src/components/Home/Card/Card.jsx
@@ -1,69 +1,137 @@
-import React from 'react';
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
 
+import { CardDetail } from "./CardDetail";
 
+const CardWrapper = styled.div`
+  width: 100%;
+  perspective: 2000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const Card = () => {
-    //Movement Animation to happen
-    const card = document.querySelector(".card");
-    const container = document.querySelector(".container");
-    //Items
-    const title = document.querySelector(".title");
-    const sneaker = document.querySelector(".sneaker img");
-    const purchase = document.querySelector(".purchase");
-    const description = document.querySelector(".info h3");
-    const sizes = document.querySelector(".sizes");
+const CardContainer = styled(motion.div)`
+  width: 285px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 25px;
+  box-shadow: 0 2px 7px 1px rgba(31, 31, 31, 0.2);
+  background-color: #1d1f21;
+  color: #fff;
+  position: relative;
+  cursor: grab;
+`;
 
-    //Moving Animation Event
-    container.addEventListener("mousemove", (e) => {
-        let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-        let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-    });
-    //Animate In
-    container.addEventListener("mouseenter", (e) => {
-        card.style.transition = "none";
-        //Popout
-        title.style.transform = "translateZ(150px)";
-        sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-        description.style.transform = "translateZ(125px)";
-        sizes.style.transform = "translateZ(100px)";
-        purchase.style.transform = "translateZ(75px)";
-    });
-    //Animate Out
-    container.addEventListener("mouseleave", (e) => {
-        card.style.transition = "all 0.5s ease";
-        card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-        //Popback
-        title.style.transform = "translateZ(0px)";
-        sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-        description.style.transform = "translateZ(0px)";
-        sizes.style.transform = "translateZ(0px)";
-        purchase.style.transform = "translateZ(0px)";
-    });
+const CircleWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  overflow: hidden;
+  border-top-right-radius: 25px;
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  width: 350px;
+  height: 350px;
+  top: -4.2em;
+  right: -10em;
+  z-index: 5;
+  background-color: #fbbe01;
+  border-radius: 50%;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1.2;
+  position: relative;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 1em 15px;
+`;
+
+const BottomContainer = styled.div`
+  display: flex;
+  flex: 0.8;
+  padding: 0 1em;
+`;
+
+const NikeText = styled.h1`
+  color: #fff;
+  text-transform: uppercase;
+  margin: 0;
+  z-index: 10;
+  font-size: 76px;
+  font-weight: 900;
+`;
+
+const ShoesWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Shoes = styled(motion.div)`
+  width: auto;
+  height: 190px;
+  z-index: 99;
+  user-select: none;
+  margin-right: 3em;
+  margin-top: 4em;
+  img {
+    width: auto;
+    height: 100%;
+    user-select: none;
+  }
+`;
+
+function Card(props) {
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+    const rotateX = useTransform(y, [-100, 100], [30, -30]);
+    const rotateY = useTransform(x, [-100, 100], [-30, 30]);
+
     return (
-        <div className="card">
-            <div className="sneaker">
-                <div className="circle"></div>
-                <img src="#" alt="#" />
-            </div>
-            <div className="info">
-                <h1 class="title">Adidas</h1>
-                <h3>FUTURE-READY TRAINERS WITH WRAPPED BOOST FOR EXCEPTION COMFORT.</h3>
-                <div className="size">
-                    <button>39</button>
-                    <button>40</button>
-                    <button className="active">42</button>
-                    <button>44</button>
-                </div>
-                <div className="purchase">
-                    <button>Purchase</button>
-                </div>
-            </div>
-        </div>
+        <CardWrapper>
+            <CardContainer
+                style={{ x, y, rotateX, rotateY, z: 100 }}
+                drag
+                dragElastic={0.16}
+                dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+                whileTap={{ cursor: "grabbing" }}
+            >
+                <TopContainer>
+                    <CircleWrapper>
+                        <Circle />
+                    </CircleWrapper>
+                    <ShoesWrapper>
+                        <Shoes
+                            style={{ x, y, rotateX, rotateY, rotate: "-25deg", z: 100000 }}
+                            drag
+                            dragElastic={0.12}
+                            whileTap={{ cursor: "grabbing" }}
+                        >
+                            <img />
+                        </Shoes>
+                    </ShoesWrapper>
+                    <NikeText>NIKE AIR</NikeText>
+                </TopContainer>
+                <BottomContainer>
+                    <CardDetail />
+                </BottomContainer>
+            </CardContainer>
+        </CardWrapper>
     );
-};
+}
 
 export default Card;
-
-
-
